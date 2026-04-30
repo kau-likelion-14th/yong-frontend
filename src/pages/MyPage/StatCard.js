3af7c524688083a388aca561c44fd9dc7b12c756
kokoya7415 <kokoya0526@naver.com>
2026-04-30 18:09:42 +0900

    [feat]: 디자인 추가 수정

--- src/pages/MyPage/StatCard.js
+++ src/pages/MyPage/StatCard.js
@@ -1,19 +1,13 @@
 import React from "react";
 
 const StatCard = ({ stats }) => {
-  const isThinText = stats.statistics.value === "토요일";
-
   return (
     <div className="stat-card">
       <div className="stat-icon">{stats.icon}</div>
       <div className="stat-title">{stats.title}</div>
       <div className="stat-value">
-        <span className={isThinText ? 'thin-text' : ''}>
-          {stats.statistics.value}
-        </span>
-        {stats.statistics.unit && (
-          <span className="stat-unit">{stats.statistics.unit}</span>
-        )}
+        {stats.statistics.value}
+        <span className="stat-unit">{stats.statistics.unit}</span>
       </div>
     </div>
   );
